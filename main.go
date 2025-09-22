8333d414b03db2808fce343fce839240d4b7e07d
julian <154890248+AbsurdlyUnique@users.noreply.github.com>
2025-09-22 07:05:34 +0200

    doorman exampled started

--- main.go
+++ main.go
@@ -1,9 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// vars
+var reader *bufio.Reader
+var age int
+var name string
+var err error
 
 func main() {
-	var message string
-	message = "hello world!"
-	fmt.Println(message)
+	args()
+}
+
+func args() {
+	// if invoke argument is given, use it as age
+	if len(os.Args) > 1 {
+		// check if input is int
+		if age, err = strconv.Atoi(os.Args[1]); err == nil {
+			os.Args[1] = strconv.Itoa(age)
+		} else {
+			fmt.Println("usage: age name")
+			os.Exit(1)
+		}
+
+		if len(os.Args) > 2 {
+			name = os.Args[2]
+		} else {
+			name = askName()
+		}
+	}
+}
+
+func askName() string {
+	reader = bufio.NewReader(os.Stdin)
+	fmt.Println("what is your name mate?")
+	name, _ := reader.ReadString('\n')
+	return name
+}
+
+func askAge() int {
+	reader = bufio.NewReader(os.Stdin)
+	fmt.Println("and how old are you?")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	age, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Errorf("invalid age: %s", input)
+		os.Exit(1)
+	}
+	return age
 }
